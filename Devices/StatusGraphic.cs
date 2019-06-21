2dd8eb750a736110aed369378073f2f0a4211331
Jon-Luke West <jonluke@homeseer.com>
2019-06-21 17:01:10 -0400

    Update StatusGraphic class

--- Devices/StatusGraphic.cs
+++ Devices/StatusGraphic.cs
@@ -2,45 +2,42 @@ using System;
 
 namespace HomeSeer.PluginSdk {
 
+    /// <summary>
+    /// Status graphics are used to specify what image is displayed for a device when its value matches certain criteria
+    /// </summary>
     [System.Reflection.Obfuscation(Exclude = true, ApplyToMembers = true)]
     [Serializable]
     public class StatusGraphic {
 
+        private bool         _isRange = false;
         private string       _graphicPath = "";
         private double       _value;
-        public  double       _rangeStart;
-        public  double       _rangeEnd;
-        public string Graphic
-        {
-            set
-            {
-                _graphicPath = value;
-            }
-        }
-        public double Value
-        {
-            get
-            {
-                return _value;
-                /*if (this.PairType == Constants.VSVGPairType.SingleValue)
-                    return _value;
-                else
-                    return _rangeStart;*/
-            }
-        }
-        public double Set_Value {
-            set
-            {
-                _value = value;
-            }
+        private double       _rangeStart;
+        private double       _rangeEnd;
+        
+        public string Graphic {
+            get => _graphicPath;
+            set => _graphicPath = value;
         }
 
-        public string GetGraphic(double Value) {
-            /*if (PairType != Constants.VSVGPairType.Range)
-                return _graphicPath;
-            if ((Value < _rangeStart) | (Value > _rangeEnd))
-                return "";*/
-            return _graphicPath;
+        public bool IsRange {
+            get => _isRange;
+            set => _isRange = value;
+        }
+
+        public double RangeStart {
+            get => _rangeStart;
+            set => _rangeStart = value;
+        }
+
+        public double RangeEnd {
+            get => _rangeEnd;
+            set => _rangeEnd = value;
+        }
+
+        public double Value {
+            get => _value;
+            set => _value = value;
         }
 
     }
