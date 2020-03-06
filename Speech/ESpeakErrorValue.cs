101e27f95053fde5d0cb6405663c6ea3ed6c85d6
Jon-Luke West <jonluke@homeseer.com>
2020-03-06 17:44:44 -0500

    Move some enums out of Constants into appropriate namespace

---
+++ Speech/ESpeakErrorValue.cs
@@ -0,0 +1,14 @@
+namespace HomeSeer.PluginSdk.Speech {
+
+    /// <summary>
+    /// PLEASE NOTE: Code related to the Speech components in HomeSeer were ported from the HS3 plugin API and
+    ///  have not been fully tested to verify full functionality from the new SDK. The Speech API may undergo
+    ///  significant changes in the near future. Please use with caution.
+    /// </summary>
+    public enum ESpeakErrorValue {
+        NoError      = 1,
+        NoClients    = 2,
+        ErrorSending = 3
+    }
+
+}
