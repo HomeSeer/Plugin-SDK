be22b1f67370d8b5d3e527ab30d9593bf5657337
Jon-Luke West <1074249+jldubz@users.noreply.github.com>
2020-03-31 09:55:56 -0400

    PSDK-59 Add PageAction classes for each defined EPageAction type

---
+++ Features/Responses/PageActions/SetTextPageAction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace HomeSeer.PluginSdk.Features.Responses.PageActions {
+    
+    /// <summary>
+    /// A <see cref="FeaturePageAction"/> used to instruct the client to set the text for an HTML element
+    /// </summary>
+    /// <remarks>Uses the <a href="https://www.w3schools.com/jquery/html_text.asp">jQuery text() method</a></remarks>
+    [JsonObject]
+    public class SetTextPageAction : FeaturePageAction {
+
+        /// <summary>
+        /// Create a new <see cref="FeaturePageAction"/> with the <see cref="EPageAction.SetText"/> action type
+        /// </summary>
+        [JsonConstructor]
+        public SetTextPageAction() {
+            PageAction = EPageAction.SetText.GetKey();
+        }
+
+        /// <summary>
+        /// Create new <see cref="FeaturePageAction"/> with the <see cref="EPageAction.SetText"/> action type using a specific JQuery selector
+        /// </summary>
+        /// <param name="selector">The JQuery selector to use</param>
+        /// <param name="text">The text to set on the element</param>
+        /// <exception cref="ArgumentNullException">Thrown if no selector is specified</exception>
+        public SetTextPageAction(string selector, string text) {
+            if (string.IsNullOrWhiteSpace(selector)) {
+                throw new ArgumentNullException(nameof(selector));
+            }
+
+            PageAction = EPageAction.SetText.GetKey();
+            Selector = selector;
+            Data = text ?? "";
+        }
+    }
+}
---
+++ Features/Responses/PageActions/SetValuePageAction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace HomeSeer.PluginSdk.Features.Responses.PageActions {
+    
+    /// <summary>
+    /// A <see cref="FeaturePageAction"/> used to instruct the client to set the value for an HTML element
+    /// </summary>
+    /// <remarks>Uses the <a href="https://www.w3schools.com/jquery/html_val.asp">jQuery val() method</a></remarks>
+    [JsonObject]
+    public class SetValuePageAction : FeaturePageAction {
+
+        /// <summary>
+        /// Create a new <see cref="FeaturePageAction"/> with the <see cref="EPageAction.SetValue"/> action type
+        /// </summary>
+        [JsonConstructor]
+        public SetValuePageAction() {
+            PageAction = EPageAction.SetValue.GetKey();
+        }
+
+        /// <summary>
+        /// Create new <see cref="FeaturePageAction"/> with the <see cref="EPageAction.SetValue"/> action type using a specific JQuery selector
+        /// </summary>
+        /// <param name="selector">The JQuery selector to use</param>
+        /// <param name="value">The value to set on the element</param>
+        /// <exception cref="ArgumentNullException">Thrown if no selector is specified</exception>
+        public SetValuePageAction(string selector, string value) {
+            if (string.IsNullOrWhiteSpace(selector)) {
+                throw new ArgumentNullException(nameof(selector));
+            }
+
+            PageAction = EPageAction.SetValue.GetKey();
+            Selector = selector;
+            Data = value ?? "";
+        }
+    }
+}
