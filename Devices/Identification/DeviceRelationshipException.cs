e62c0b73ccf9e06af31e93b867d66b44029e25f8
Chris <chris@homeseer.com>
2020-12-22 14:13:34 -0500

    PSDK-117 Fix exception definitions in order for HSCF.dll to be able to deserialize them.

--- Devices/Identification/DeviceRelationshipException.cs
+++ Devices/Identification/DeviceRelationshipException.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace HomeSeer.PluginSdk.Devices.Identification {
 
     /// <summary>
     /// Represents errors occuring while trying to set the relationship of a device or feature
     /// </summary>
+    [Serializable]
     public class DeviceRelationshipException : Exception {
         
         /// <summary>
@@ -19,6 +21,14 @@ namespace HomeSeer.PluginSdk.Devices.Identification {
         /// <param name="message">The error message</param>
         /// <param name="innerException">An <see cref="Exception"/> that caused this exception</param>
         public DeviceRelationshipException(string message, Exception innerException) : base(message, innerException) { }
+
+        /// <summary>
+        /// Special constructor used for deserialization. 
+        /// This is mandatory in order for HSCF to be able to deserialize this exception.
+        /// </summary>
+        /// <param name="info">The data to deserialize from</param>
+        /// <param name="context">The context of the source stream</param>
+        protected DeviceRelationshipException(SerializationInfo info, StreamingContext context) : base(info, context) { }
 
     }
 
