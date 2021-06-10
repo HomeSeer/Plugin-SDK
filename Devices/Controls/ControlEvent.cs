d82357714f8ded99b0aafe2cb4ba8fb9569051fa
Jon-Luke West <jonluke@homeseer.com>
2021-06-10 12:53:27 -0400

    PSDK-158 JLW Update documentation on ControlEvent

--- Devices/Controls/ControlEvent.cs
+++ Devices/Controls/ControlEvent.cs
@@ -2,6 +2,11 @@ using System;
 
 namespace HomeSeer.PluginSdk.Devices.Controls {
 
+    /// <summary>
+    /// The description of an event representing a controlled change of a <see cref="HsFeature"/>.
+    /// </summary>
+    /// <seealso cref="StatusControl"/>
+    /// <seealso cref="HsFeature.CreateControlEvent"/>
     [System.Reflection.Obfuscation(Exclude = true, ApplyToMembers = true)]
     [Serializable]
     public class ControlEvent {
@@ -14,34 +19,62 @@ namespace HomeSeer.PluginSdk.Devices.Controls {
         private int _ref;
         private int _index = -1;
         
+        /// <summary>
+        /// A unique index representing the CAPIControl in HomeSeer.
+        /// </summary>
+        /// <remarks>This is a carry over from the legacy API. It should not need to be used anymore and is maintained for compatibility.</remarks>
+        [Obsolete("This is a carry over from the legacy API. It should not need to be used anymore and is maintained for compatibility.", false)]
         public int CCIndex {
             get => _index;
             set => _index = value;
         }
         
+        /// <summary>
+        /// The <see cref="AbstractHsDevice.Ref"/> of the <see cref="HsFeature"/> being controlled.
+        /// </summary>
         public int TargetRef {
             get => _ref;
             set => _ref = value;
         }
 
+        /// <summary>
+        /// A human-readable string representing the label to display on the <see cref="HsFeature"/> for this event.
+        /// </summary>
         public string Label {
             get => _label;
             set => _label = value;
         }
         
+        /// <summary>
+        /// The <see cref="EControlType"/> of the <see cref="StatusControl"/> used to generate this event.
+        /// </summary>
         public EControlType ControlType {
             get => _controlType;
             set => _controlType = value;
         }
         
+        /// <summary>
+        /// The <see cref="EControlUse"/> of the <see cref="StatusControl"/> used to generate this event.
+        /// </summary>
         public EControlUse ControlUse {
             get => _controlUse;
             set => _controlUse = value;
         }
+        
+        /// <summary>
+        /// The target value for the event. This is the desired <see cref="AbstractHsDevice.Value"/>
+        /// </summary>
         public double ControlValue {
             get => _controlValue;
             set => _controlValue = value;
         }
+        
+        /// <summary>
+        /// A string containing control data for the event. This is used for <see cref="EControlType.ColorPicker"/>s
+        /// </summary>
+        /// <remarks>
+        /// This is NOT the <see cref="Label"/> to display in the <see cref="AbstractHsDevice.Status"/> of the <see cref="HsFeature"/>. This is not displayed anywhere.
+        /// </remarks>
         public string ControlString {
             get => _controlString;
             set {
@@ -53,6 +86,10 @@ namespace HomeSeer.PluginSdk.Devices.Controls {
             }
         }
     
+        /// <summary>
+        /// Create a new <see cref="ControlEvent"/> for a <see cref="AbstractHsDevice.Ref"/>
+        /// </summary>
+        /// <param name="devRef">The <see cref="AbstractHsDevice.Ref"/> of the <see cref="HsFeature"/> being controlled</param>
         public ControlEvent(int devRef) {
             _ref = devRef;
         }
