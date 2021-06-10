d853ada31e8bf16071d95932f47adc511c1f46c1
Jon-Luke West <jonluke@homeseer.com>
2021-06-10 12:58:28 -0400

    PSDK-158 JLW Update documentation on StatusControlCollection

--- Devices/Controls/StatusControlCollection.cs
+++ Devices/Controls/StatusControlCollection.cs
@@ -5,6 +5,14 @@ using System.Reflection;
 
 namespace HomeSeer.PluginSdk.Devices.Controls {
 
+    /// <summary>
+    /// A managed collection of <see cref="StatusControl"/>s
+    /// </summary>
+    /// <remarks>
+    /// This is a <see cref="SortedDictionary{TKey,TValue}"/> where <see cref="StatusControl.TargetValue"/> or
+    ///  <see cref="ValueRange.Min"/> of <see cref="StatusControl.TargetRange"/> is used as the key.
+    ///  This is used to ensure that there is only one <see cref="StatusControl"/> per <see cref="AbstractHsDevice.Value"/>
+    /// </remarks>
     [Obfuscation(Exclude = true, ApplyToMembers = true)]
     [Serializable]
     public class StatusControlCollection {
@@ -13,6 +21,11 @@ namespace HomeSeer.PluginSdk.Devices.Controls {
         
         //Create
         
+        /// <summary>
+        /// Add a <see cref="StatusControl"/> to the collection
+        /// </summary>
+        /// <param name="statusControl">A <see cref="StatusControl"/> to add. It must not target a value that is already handled by the collection.</param>
+        /// <exception cref="ArgumentException">Thrown if the <paramref name="statusControl"/> targets a value that is already handled in the collection.</exception>
         public void Add(StatusControl statusControl) {
             if (Contains(statusControl)) {
                 throw new ArgumentException("A status control covering all or a portion of that value range already exists");
@@ -21,6 +34,11 @@ namespace HomeSeer.PluginSdk.Devices.Controls {
             _statusControls.Add(statusControl.IsRange ? statusControl.TargetRange.Min : statusControl.TargetValue, statusControl);
         }
 
+        /// <summary>
+        /// Add multiple <see cref="StatusControl"/>s to the collection
+        /// </summary>
+        /// <param name="statusControls">A collection of <see cref="StatusControl"/>s to add. Make sure there is only one <see cref="StatusControl"/> handling each value.</param>
+        /// <exception cref="ArgumentException">Thrown if any element in <paramref name="statusControls"/> targets a value that is already handled in the collection.</exception>
         public void AddRange(List<StatusControl> statusControls) {
 
             foreach (var statusControl in statusControls) {
@@ -35,6 +53,11 @@ namespace HomeSeer.PluginSdk.Devices.Controls {
         
         //Read
         
+        /// <summary>
+        /// Get the <see cref="StatusControl"/> in the collection that handles the specified value
+        /// </summary>
+        /// <param name="value">The value associated with the desired <see cref="StatusControl"/></param>
+        /// <exception cref="KeyNotFoundException">Thrown when no <see cref="StatusControl"/> is found that handles the specified <paramref name="value"/></exception>
         public StatusControl this[double value] {
             get {
                 try {
@@ -66,11 +89,24 @@ namespace HomeSeer.PluginSdk.Devices.Controls {
             }
         }
         
-        public bool Contains(StatusControl statusControl) 
-        {    
+        /// <summary>
+        /// Determine if a <see cref="StatusControl"/> is already in the managed collection
+        /// </summary>
+        /// <remarks>
+        /// This check is based on <see cref="StatusControl.TargetValue"/> or the <see cref="ValueRange.Min"/> of
+        ///  <see cref="StatusControl.TargetRange"/>
+        /// </remarks>
+        /// <param name="statusControl">The <see cref="StatusControl"/> to search for</param>
+        /// <returns><see langword="True"/> if the <paramref name="statusControl"/> is in the collection, <see langword="False"/> if it is not</returns>
+        public bool Contains(StatusControl statusControl) {    
             return ContainsValue(statusControl.IsRange ? statusControl.TargetRange.Min : statusControl.TargetValue);
         }
 
+        /// <summary>
+        /// Determine if a value is handled by the collection
+        /// </summary>
+        /// <param name="value">The value to check for</param>
+        /// <returns><see langword="True"/> if the value is handled, <see langword="False"/> if it is not</returns>
         public bool ContainsValue(double value) {
             try {
 
@@ -92,6 +128,12 @@ namespace HomeSeer.PluginSdk.Devices.Controls {
             }
         }
         
+        /// <summary>
+        /// Get a list of <see cref="StatusControl"/>s that handle a given range of values
+        /// </summary>
+        /// <param name="min">The smallest number in the range</param>
+        /// <param name="max">The largest number in the range</param>
+        /// <returns>A List of <see cref="StatusControl"/>s</returns>
         public List<StatusControl> GetControlsForRange(double min, double max) {
             
             var foundStatusControls = new List<StatusControl>();
@@ -105,15 +147,22 @@ namespace HomeSeer.PluginSdk.Devices.Controls {
             return foundStatusControls;
         }
         
-        public List<StatusControl> Values => _statusControls.Values.Cast<StatusControl>().ToList();
+        /// <summary>
+        /// Get an unordered list of the <see cref="StatusControl"/>s in the collection
+        /// </summary>
+        public List<StatusControl> Values => _statusControls.Values.ToList();
         
+        /// <summary>
+        /// The number of <see cref="StatusControl"/>s in the collection
+        /// </summary>
         public int Count => _statusControls?.Count ?? 0;
-        
-        //Update
-        
-        
+
         //Delete
 
+        /// <summary>
+        /// Remove the <see cref="StatusControl"/> that handles the specified <paramref name="value"/>
+        /// </summary>
+        /// <param name="value">The value handled by the <see cref="StatusControl"/> to remove</param>
         public void RemoveKey(double value) {
             if (!_statusControls.ContainsKey(value)) {
                 return;
@@ -122,6 +171,16 @@ namespace HomeSeer.PluginSdk.Devices.Controls {
             _statusControls.Remove(value);
         }
         
+        /// <summary>
+        /// Remove a <see cref="StatusControl"/> from the collection
+        /// </summary>
+        /// <remarks>
+        /// A key is determined by the <see cref="StatusControl.TargetValue"/> or the <see cref="ValueRange.Min"/> of
+        ///  <see cref="StatusControl.TargetRange"/> of <paramref name="statusControl"/>.
+        ///  The found item is then compared by hash code to ensure they are the
+        ///  same.
+        /// </remarks>
+        /// <param name="statusControl">A <see cref="StatusControl"/> to remove</param>
         public void Remove(StatusControl statusControl) {
             var itemKey = statusControl.IsRange ? statusControl.TargetRange.Min : statusControl.TargetValue;
             var itemToDelete = this[itemKey];
@@ -131,6 +190,9 @@ namespace HomeSeer.PluginSdk.Devices.Controls {
             }
         }
 
+        /// <summary>
+        /// Remove all <see cref="StatusControl"/>s in the collection
+        /// </summary>
         public void RemoveAll() {
             _statusControls = new SortedDictionary<double, StatusControl>();
         }
