96327508c86f7ac77a261ad35125e39fd9be6af3
Jon-Luke West <jonluke@homeseer.com>
2022-02-04 13:36:28 -0500

    PSDK-182 JLW Update documentation on Types.EHsSystemEvent.

--- Types/EHsSystemEvent.cs
+++ Types/EHsSystemEvent.cs
@@ -1,29 +1,115 @@
 namespace HomeSeer.PluginSdk.Types {
     
+    /// <summary>
+    /// Types of system events.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// System events represent changes in the system that plugins can react to.
+    ///  They are not the same thing as Events configured by users.
+    /// </para>
+    /// <para>
+    /// This replaces <see cref="Constants.HSEvent"/>
+    /// </para>
+    /// </remarks>
     public enum EHsSystemEvent {
         // For HSEvent callbacks
         
         // X10 = 1
+        
+        //TODO document Log
+        /// <summary>
+        /// 
+        /// </summary>
         Log = 2,
+        
         // STATUS_CHANGE = 4       ' a device changed status or value
+        
+        //TODO document Audio
+        /// <summary>
+        /// 
+        /// </summary>
         Audio = 8,
+        
         // X10_TRANSMIT = &H10S
+        
+        //TODO document ConfigChange
+        /// <summary>
+        /// 
+        /// </summary>
         ConfigChange   = 0x20,
+        
+        //TODO document StringChange
+        /// <summary>
+        /// 
+        /// </summary>
         StringChange   = 0x40,
+        
+        //TODO document SpeakerConnect
+        /// <summary>
+        /// 
+        /// </summary>
         SpeakerConnect = 0x80,
+        
+        //TODO document CallerId
+        /// <summary>
+        /// 
+        /// </summary>
         CallerId       = 0x100,
+        
         // ZWAVE = &H200
-        ValueChange = 0x400, // rjh added in 2.2.0.3
+        
+        //TODO document ValueChange
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// rjh added in 2.2.0.3
+        /// </remarks>
+        ValueChange = 0x400,
+        
         /// <summary>
         /// A device has been deleted
         /// </summary>
         DeleteDevice = 0x600,
-        ValueSet    = 0x800, // rjh added in 3.0.0.292
+        
+        //TODO document ValueSet
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// rjh added in 3.0.0.292
+        /// </remarks>
+        ValueSet    = 0x800,
+        
         // STATUS_ONLY_CHANGE = &H800 ' rjh added in 2.2.0.10
+        
+        //TODO document VoiceRec
+        /// <summary>
+        /// 
+        /// </summary>
         VoiceRec          = 0x1000,
+        
+        //TODO document SetupChange
+        /// <summary>
+        /// 
+        /// </summary>
         SetupChange       = 0x2000,
+        
+        //TODO document RunScriptSpecial
+        /// <summary>
+        /// 
+        /// </summary>
         RunScriptSpecial = 0x4000,
-        Generic            = 0x8000 // RVCT added 3/10/08, 2.2.0.76ish
+        
+        //TODO document Generic
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// RVCT added 3/10/08, 2.2.0.76ish
+        /// </remarks>
+        Generic            = 0x8000
 
     }
 
