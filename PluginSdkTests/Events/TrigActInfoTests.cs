f3e0a2487923ddb6e98514364b3fbf834d870992
Jon-Luke West <jonluke@homeseer.com>
2022-02-21 15:11:27 -0500

    PSDK-138 JLW move TestData class to project root so it can be used by any tests.

--- PluginSdkTests/Events/TrigActInfoTests.cs
+++ PluginSdkTests/Events/TrigActInfoTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using HomeSeer.PluginSdkTests;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 
@@ -63,36 +64,6 @@ namespace HomeSeer.PluginSdk.Events {
                 Console.WriteLine($"Error Serializing object: {ex.Message}");
             }
             return Array.Empty<byte>();
-        }
-
-        [Serializable]
-        public class TestData {
-            public string Key { get; set; }
-            public int Value { get; set; }
-
-            public TestData() { }
-
-            public TestData(string key, int value) {
-                Key = key;
-                Value = value;
-            }
-
-            protected bool Equals(TestData other) {
-                return Key == other.Key && Value == other.Value;
-            }
-
-            public override bool Equals(object obj) {
-                if (ReferenceEquals(null, obj)) return false;
-                if (ReferenceEquals(this, obj)) return true;
-                if (obj.GetType() != GetType()) return false;
-                return Equals((TestData)obj);
-            }
-
-            public override int GetHashCode() {
-                unchecked {
-                    return ((Key != null ? Key.GetHashCode() : 0) * 397) ^ Value;
-                }
-            }
         }
 
     }
