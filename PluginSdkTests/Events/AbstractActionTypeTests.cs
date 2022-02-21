73df0388caef62c7bee6d219555e5a4c8d610fac
Jon-Luke West <jonluke@homeseer.com>
2022-02-21 10:28:10 -0500

    PSDK-222 JLW Add event test fixtures. Add TrigActInfo tests. Add TestActionType for unit tests. Start AbstractActionTypeTests

--- PluginSdkTests/Events/AbstractActionTypeTests.cs
+++ PluginSdkTests/Events/AbstractActionTypeTests.cs
@@ -2,12 +2,27 @@ using NUnit.Framework;
 
 namespace HomeSeer.PluginSdk.Events {
 
-    [TestFixture]
+    [TestFixture(
+        TestOf = typeof(AbstractActionType),
+        Description = "Tests of the AbstractActionType class to ensure it behaves as expected under normal conditions.",
+        Author = "JLW")]
     public class AbstractActionTypeTests {
 
         //TODO Constructors
         
         //TODO LogDebug
+        public void LogDebug_Get_ReturnsDefault() {
+            var testActionType = new TestActionType();
+            Assert.AreEqual(false, testActionType.LogDebug);
+        }
+
+        public void LogDebug_Set_SetsLogDebug(bool logDebug) {
+            var testActionType = new TestActionType {
+                LogDebug = logDebug
+            };
+            Assert.AreEqual(logDebug, testActionType.LogDebug);
+        }
+
         //TODO ActionListener
         //TODO Id
         //TODO EventRef
