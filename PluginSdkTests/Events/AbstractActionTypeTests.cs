f56204c43df2136af3645a6dc638d01cdba67f2c
Jon-Luke West <jonluke@homeseer.com>
2022-02-21 08:35:12 -0500

    PSDK-221 JLW add event test classes

---
+++ PluginSdkTests/Events/AbstractActionTypeTests.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+
+namespace HomeSeer.PluginSdk.Events {
+
+    [TestFixture]
+    public class AbstractActionTypeTests {
+
+        //TODO Constructors
+        
+        //TODO LogDebug
+        //TODO ActionListener
+        //TODO Id
+        //TODO EventRef
+        //TODO Data
+        //TODO Name
+        //TODO ToHtml
+        //TODO ProcessPostData
+        
+        //TODO IsFullyConfigured
+        //TODO GetPrettyString
+        //TODO OnRunAction
+        //TODO ReferencesDeviceOrFeature
+
+    }
+
+}
---
+++ PluginSdkTests/Events/TrigActInfoTests.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+
+namespace HomeSeer.PluginSdk.Events {
+
+    [TestFixture]
+    public class TrigActInfoTests {
+        
+        //TODO DeserializeLegacyObject
+        // inData is blank return null
+        // invalid cast exception return null
+        // deserialization exception caused by bad data return null
+        // valid inData returns expected objects
+
+    }
+
+}
