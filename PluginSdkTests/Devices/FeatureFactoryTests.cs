2f6063c78ca0e8ca5af837e553855e2eda41b9ca
Chris <chris@homeseer.com>
2022-05-05 09:20:46 -0400

    PSDK-268 Add tests for adding a single valid StatusGraphic to a FeatureFactory

--- PluginSdkTests/Devices/FeatureFactoryTests.cs
+++ PluginSdkTests/Devices/FeatureFactoryTests.cs
@@ -19,9 +19,91 @@ namespace HomeSeer.PluginSdk.Devices {
             _ff = FeatureFactory.CreateFeature("TestPluginId");
         }
 
+        private static IEnumerable<StatusGraphic> StatusGraphicTestCaseSource() {
+            var sg = new StatusGraphic(IMAGES_STATUS_DIR + "Thermometer-00.png", -100, -18);
+            sg.TargetRange.DecimalPlaces = 1;
+            sg.TargetRange.Suffix = " °C";
+            yield return sg;
+
+            sg = new StatusGraphic(IMAGES_STATUS_DIR + "Thermometer-10.png", -18 + EPSILON, -12);
+            sg.TargetRange.DecimalPlaces = 1;
+            sg.TargetRange.Suffix = " °C";
+            yield return sg;
+
+            sg = new StatusGraphic(IMAGES_STATUS_DIR + "Thermometer-20.png", -12 + EPSILON, -7);
+            sg.TargetRange.DecimalPlaces = 1;
+            sg.TargetRange.Suffix = " °C";
+            yield return sg;
+
+            sg = new StatusGraphic(IMAGES_STATUS_DIR + "Thermometer-30.png", -7 + EPSILON, -1);
+            sg.TargetRange.DecimalPlaces = 1;
+            sg.TargetRange.Suffix = " °C";
+            yield return sg;
+
+            sg = new StatusGraphic(IMAGES_STATUS_DIR + "Thermometer-40.png", -1 + EPSILON, 4);
+            sg.TargetRange.DecimalPlaces = 1;
+            sg.TargetRange.Suffix = " °C";
+            yield return sg;
+
+            sg = new StatusGraphic(IMAGES_STATUS_DIR + "Thermometer-50.png", 4 + EPSILON, 10);
+            sg.TargetRange.DecimalPlaces = 1;
+            sg.TargetRange.Suffix = " °C";
+            yield return sg;
+
+            sg = new StatusGraphic(IMAGES_STATUS_DIR + "Thermometer-60.png", 10 + EPSILON, 16);
+            sg.TargetRange.DecimalPlaces = 1;
+            sg.TargetRange.Suffix = " °C";
+            yield return sg;
+
+            sg = new StatusGraphic(IMAGES_STATUS_DIR + "Thermometer-70.png", 16 + EPSILON, 21);
+            sg.TargetRange.DecimalPlaces = 1;
+            sg.TargetRange.Suffix = " °C";
+            yield return sg;
+
+            sg = new StatusGraphic(IMAGES_STATUS_DIR + "Thermometer-80.png", 21 + EPSILON, 27);
+            sg.TargetRange.DecimalPlaces = 1;
+            sg.TargetRange.Suffix = " °C";
+            yield return sg;
+
+            sg = new StatusGraphic(IMAGES_STATUS_DIR + "Thermometer-90.png", 27 + EPSILON, 32);
+            sg.TargetRange.DecimalPlaces = 1;
+            sg.TargetRange.Suffix = " °C";
+            yield return sg;
+
+            sg = new StatusGraphic(IMAGES_STATUS_DIR + "Thermometer-100.png", 32 + EPSILON, 38);
+            sg.TargetRange.DecimalPlaces = 1;
+            sg.TargetRange.Suffix = " °C";
+            yield return sg;
+
+            sg = new StatusGraphic(IMAGES_STATUS_DIR + "Thermometer-110.png", 38 + EPSILON, 100);
+            sg.TargetRange.DecimalPlaces = 1;
+            sg.TargetRange.Suffix = " °C";
+            yield return sg;
+
+            sg = new StatusGraphic(IMAGES_STATUS_DIR + "heating.png", 0, "Heat");
+            yield return sg;
+
+            sg = new StatusGraphic(IMAGES_STATUS_DIR + "cooling.png", 1, "Cool");
+            yield return sg;
+
+            sg = new StatusGraphic(IMAGES_STATUS_DIR + "auto-mode.png", 2, "Auto");
+            yield return sg;
+            
+            sg = new StatusGraphic(IMAGES_STATUS_DIR + "off.gif", 3, "Off");
+            yield return sg;
+        }
+
+        [TestCaseSource(nameof(StatusGraphicTestCaseSource))]
+        [Description("Add a Single Valid StatusGraphic")]
+        public void AddGraphic_Single_Valid(StatusGraphic sg) {
+            Assert.DoesNotThrow(() => {
+                _ff.AddGraphic(sg);
+            });
+        }
+
         [Test]
-        [Description("Add Valid StatusGraphics that target range of values")]
-        public void AddGraphic_Valid_Ranges() {
+        [Description("Add Multiple Valid StatusGraphics that target range of values")]
+        public void AddGraphic_Multiple_Valid_Ranges() {
 
             var sg = new StatusGraphic(IMAGES_STATUS_DIR + "Thermometer-00.png", -100, -18);
             sg.TargetRange.DecimalPlaces = 1;
@@ -85,8 +167,8 @@ namespace HomeSeer.PluginSdk.Devices {
         }
 
         [Test]
-        [Description("Add Valid StatusGraphics that target single values")]
-        public void AddGraphic_Valid_SingleValues() {
+        [Description("Add Multiple Valid StatusGraphics that target single values")]
+        public void AddGraphic_Multiple_Valid_SingleValues() {
 
             var sg = new StatusGraphic(IMAGES_STATUS_DIR + "heating.png", 0, "Heat");
             _ff.AddGraphic(sg);
@@ -100,7 +182,7 @@ namespace HomeSeer.PluginSdk.Devices {
 
         [Test]
         [Description("Add overlapping StatusGraphics that target range of values and expect an exception to be thrown.")]
-        public void AddGraphic_Invalid_Ranges_Throws() {
+        public void AddGraphic_Multiple_Invalid_Ranges_Throws() {
 
             var sg = new StatusGraphic(IMAGES_STATUS_DIR + "Thermometer-00.png", -100, 50);
             _ff.AddGraphic(sg);
@@ -113,7 +195,7 @@ namespace HomeSeer.PluginSdk.Devices {
 
         [Test]
         [Description("Add multiple StatusGraphics that target the same single value and expect an exception to be thrown.")]
-        public void AddGraphic_Invalid_SingleValues_Throws() {
+        public void AddGraphic_Multiple_Invalid_SingleValues_Throws() {
 
             var sg = new StatusGraphic(IMAGES_STATUS_DIR + "heating.png", 0, "Heat");
             _ff.AddGraphic(sg);
@@ -135,7 +217,5 @@ namespace HomeSeer.PluginSdk.Devices {
                 _ff.AddGraphic(null);
             });
         }
-
-
     }
 }
