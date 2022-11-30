d1ffa4d1525ae4541f430bbb3dd995270b760315
Chris <chris@homeseer.com>
2022-11-30 13:32:41 -0500

    PSDK-295 Add vertical and horizontal alignment properties to GridRow class

--- Jui/Views/EColSize.cs
+++ Jui/Views/EColSize.cs
@@ -1,71 +1,69 @@
 namespace HomeSeer.Jui.Views {
 
-	/// <summary>
-	/// The poissible sizes for a column in the <a href="https://mdbootstrap.com/docs/standard/layout/grid/">bootstrap grid system</a>
-	/// </summary>
-	public enum EColSize 
-	{
-		/// <summary>
-		/// None
-		/// </summary>
-		None = -1,
-		/// <summary>
-		/// Equal-width column
-		/// </summary>
-		Col = 0,
-		/// <summary>
-		/// Column of size 1
-		/// </summary>
-		Col1 = 1,
-		/// <summary>
-		/// Column of size 2
-		/// </summary>
-		Col2 = 2,
-		/// <summary>
-		/// Column of size 3
-		/// </summary>
-		Col3 = 3,
-		/// <summary>
-		/// Column of size 4
-		/// </summary>
-		Col4 = 4,
-		/// <summary>
-		/// Column of size 5
-		/// </summary>
-		Col5 = 5,
-		/// <summary>
-		/// Column of size 6
-		/// </summary>
-		Col6 = 6,
-		/// <summary>
-		/// Column of size 7
-		/// </summary>
-		Col7 = 7,
-		/// <summary>
-		/// Column of size 8
-		/// </summary>
-		Col8 = 8,
-		/// <summary>
-		/// Column of size 9
-		/// </summary>
-		Col9 = 9,
-		/// <summary>
-		/// Column of size 10
-		/// </summary>
-		Col10 = 10,
-		/// <summary>
-		/// Column of size 11
-		/// </summary>
-		Col11 = 11,
-		/// <summary>
-		/// Column of size 12
-		/// </summary>
-		Col12 = 12,
-		/// <summary>
-		/// Size based on the natural width of their content
-		/// </summary>
-		Auto = 13,
-
-	}
+    /// <summary>
+    /// The possible sizes for a column in the <a href="https://mdbootstrap.com/docs/standard/layout/grid/">bootstrap grid system</a>
+    /// </summary>
+    public enum EColSize {
+        /// <summary>
+        /// None
+        /// </summary>
+        None = -1,
+        /// <summary>
+        /// Equal-width column
+        /// </summary>
+        Col = 0,
+        /// <summary>
+        /// Column of size 1
+        /// </summary>
+        Col1 = 1,
+        /// <summary>
+        /// Column of size 2
+        /// </summary>
+        Col2 = 2,
+        /// <summary>
+        /// Column of size 3
+        /// </summary>
+        Col3 = 3,
+        /// <summary>
+        /// Column of size 4
+        /// </summary>
+        Col4 = 4,
+        /// <summary>
+        /// Column of size 5
+        /// </summary>
+        Col5 = 5,
+        /// <summary>
+        /// Column of size 6
+        /// </summary>
+        Col6 = 6,
+        /// <summary>
+        /// Column of size 7
+        /// </summary>
+        Col7 = 7,
+        /// <summary>
+        /// Column of size 8
+        /// </summary>
+        Col8 = 8,
+        /// <summary>
+        /// Column of size 9
+        /// </summary>
+        Col9 = 9,
+        /// <summary>
+        /// Column of size 10
+        /// </summary>
+        Col10 = 10,
+        /// <summary>
+        /// Column of size 11
+        /// </summary>
+        Col11 = 11,
+        /// <summary>
+        /// Column of size 12
+        /// </summary>
+        Col12 = 12,
+        /// <summary>
+        /// Size based on the natural width of their content
+        /// </summary>
+        Auto = 13,
+    }
 
 }
---
+++ Jui/Views/EVerticalAlignment.cs
@@ -0,0 +1,25 @@
+namespace HomeSeer.Jui.Views {
+
+    /// <summary>
+    /// The possible behaviors for the vertical alignment of items in a row 
+    /// </summary>
+    public enum EVerticalAlignment {
+        /// <summary>
+        /// None
+        /// </summary>
+        None = -1,
+        /// <summary>
+        /// Items are placed at the top of the row
+        /// </summary>
+        AlignItemsStart = 0,
+        /// <summary>
+        /// Items are placed at the bottom of the row
+        /// </summary>
+        AlignItemsEnd = 1,
+        /// <summary>
+        /// Items are centered in the row
+        /// </summary>
+        AlignItemsCenter = 2,
+    }
+
+}
